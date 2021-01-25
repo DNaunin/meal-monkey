087362fa7a6bb2b02b87217f08412c3d9866016c
DNaunin <d.naunin@gmail.com>
2021-01-25 16:16:40 +0100

    create buttons

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,11 +1,13 @@
 import "./button.css";
-import buttonText from "./button-text.html";
-import buttonLogo from "./button-logo.html";
+import buttonFilled from "./button-filled.html";
+import buttonBorder from "./button-bord.html";
+import buttonFacebook from "./button-fb.html";
 
 export default {
   title: "Components/Button",
   parameters: { layout: "centered" },
 };
 
-export const withText = () => buttonText;
-export const withLogo = () => buttonLogo;
+export const withFilling = () => buttonFilled;
+export const withBorder = () => buttonBorder;
+export const withFBLogin = () => buttonFacebook;
